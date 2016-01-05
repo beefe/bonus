3c609ec92e738fe3b1ee12212e4bb7f4ff5ac026
xwenliang <wenliang.web@gmail.com>
2016-01-05 23:09:39 +0800

    3d box

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,21 +1,19 @@
 'use strict';
 
 var $ = require('jquery');
-var $box = $('.box');
-var tpl = '';
-for(var i=0;i<300;i++){
-	tpl += '<li><span></span></li>'
-}
-$box.append(tpl);
-var $spans = $box.find('span');
-//先打散，to do
-
-setTimeout(function(){
-	$spans.each(function(key, span){
-		var $span = $(span);
-		$span.css({
-			left: '1px',
-			top: '1px'
-		});
-	});
+var scrollPage = require('scrollPageCube');
+scrollPage({
+	par: $('.js-container').find('.scroll-page-cube-perspective')
 });
+
+var $box = $('.js-box');
+
+
+// var $box = $('.box');
+// var tpl = '';
+// for(var i=0;i<300;i++){
+// 	tpl += '<li><span></span></li>'
+// }
+// $box.append(tpl);
+// var $spans = $box.find('span');
+//先打散，to do
